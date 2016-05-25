e67c07eb6be33ca29948532533081531eff9ec15
Sangheun Lee <aisiterugh@snu.ac.kr>
2016-05-25 15:07:28 +0900

    settings_fixed

--- exercise1/settings.py
+++ exercise1/settings.py
@@ -119,7 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-settings.STATIC_URL = '/static/'
-settings.STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-settings.MEDIA_URL = '/media/'
-settings.MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
